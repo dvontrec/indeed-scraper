0530e816be8d5b3d899e9b8ba53ecf444752d2f9
Dvontre Coleman <dvontrec@gmail.com>
2019-05-26 13:11:21 -0400

    Update scraper / writer code

--- jobs.py
+++ jobs.py
@@ -3,31 +3,40 @@ from bs4 import BeautifulSoup
 from csv import writer
 
 url = "https://www.indeed.com/jobs?q=software+engineer&l=Louisville%2C+KY&explvl=entry_level"
+# Set to hold jobs to avoid duplicates
+jobSet = set()
 
-with open("jobs.csv", "w") as file:
-  csv_writer = writer(file)
-  csv_writer.writerow(["applied", "original", "title",
-                       "company", "summary", "link"])
-
-  for count in range(0, 18):
+# Loops through the first 18 pages of linkedin
+for count in range(0, 18):
     results = requests.get(url)
     soup = BeautifulSoup(results.text, "html.parser")
     jobResults = soup.select("#resultsCol .result")
     nextL = soup.select(".pagination a")
     for job in jobResults:
-      original = 0
-      anchor = job.select("a")
-      title = anchor[0]["title"]
-      link = "https://www.indeed.com/" + anchor[0]["href"]
-      job_page = requests.get(link)
-      job_soup = BeautifulSoup(job_page.text, "html.parser")
-      postlink = job_soup.select("#originalJobLinkContainer > a")
-      if postlink:
-        link = postlink[0]["href"]
-        original = "1"
-      company = job.select(".company")[0].text.strip()
-      summary = job.select(".summary")[0].text.strip()
-      csv_writer.writerow(["", original, title, company, summary, link])
+        original = 0
+        anchor = job.select("a")
+        title = anchor[0]["title"]
+        link = "https://www.indeed.com/" + anchor[0]["href"]
+        job_page = requests.get(link)
+        job_soup = BeautifulSoup(job_page.text, "html.parser")
+        postlink = job_soup.select("#originalJobLinkContainer > a")
+        if postlink:
+            link = postlink[0]["href"]
+            original = "1"
+        company = job.select(".company")[0].text.strip()
+        summary = job.select(".summary")[0].text.strip()
+        fullPost = ["", str(original), title, company, summary, link]
+        joinedPost = ",".join(fullPost)
+        jobSet.add(joinedPost)
+    url = "https://www.indeed.com/" + nextL[len(nextL)-1]["href"]
 
-    url = "https://www.indeed.com/jobs?q=software+engineer&l=Louisville%2C+KY&explvl=entry_level" + \
-        nextL[len(nextL)-1]["href"]
+# Creates a new CSV file
+out = open("jobs.csv", "w")
+
+# write the headers for the csv
+["applied", "original", "title", "company", "summary", "link"]
+out.write("applied,original,title,company,summary, link\n")
+
+# adds each address as a line in the csv file
+for job in jobSet:
+    out.write(job + "\n")
--- sql_writer.py
+++ sql_writer.py
@@ -3,57 +3,48 @@ from csv import reader
 import os
 
 # Sets up the connection object
-connectionObject = pymysql.connect(host="",
-                                   user="",
-                                   password="",
-                                   db="")
-
+connectionObject = pymysql.connect(host=os.environ["HOST"],
+                                   user=os.environ["USER"],
+                                   password=os.environ["PASSWORD"],
+                                   db=os.environ["DATABASE"])
 # Sets up the jobs array as an empty array
 jobs = []
 # opens jobs.csv and adds each job row to the list
 with open("jobs.csv") as csv_file:
-  csv_reader = reader(csv_file)
-  # MOves ahead one step to skip headings
-  next(csv_reader)
-  for row in csv_reader:
-    jobs.append(row)
-
+    csv_reader = reader(csv_file)
+    # MOves ahead one step to skip headings
+    next(csv_reader)
+    for row in csv_reader:
+        jobs.append(row)
+# Try stuff
 try:
+    # Create a cursor object
+    cursorObject = connectionObject.cursor()
+    # If the jobs table exists drop it
+    sqlQ = "DROP TABLE IF EXISTS Jobs"
+    connectionObject.commit()
+    cursorObject.execute(sqlQ)
 
-  # Create a cursor object
+    # SQL query string
+    sqlQuery = "Create TABLE Jobs(id INT PRIMARY KEY AUTO_INCREMENT, applied BOOLEAN DEFAULT FALSE, direct BOOLEAN,  title VARCHAR(255), company VARCHAR(255), summary TEXT(1000), url TEXT(1000))"
 
-  cursorObject = connectionObject.cursor()
+    # Execute the sqlQuery
+    cursorObject.execute(sqlQuery)
+    # Uses list comprehension to filter out revature jobs because they are training courses that require a degree
+    jobs = [job for job in jobs if job[3] != "Revature"]
+    # loops through all jobs in the jobs array
+    for job in jobs:
+        sql = 'INSERT into Jobs(direct, title, company, summary, url) VALUES(%s, "%s", "%s", "%s", "%s");' % (job[1], job[2], job[3], job[4], job[5])
+        cursorObject.execute(sql)
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+        connectionObject.commit()
 
-  sqlQ = "DROP TABLE IF EXISTS Jobs"
-  connectionObject.commit()
-  cursorObject.execute(sqlQ)
-
-  # SQL query string
-  sqlQuery = "Create TABLE Jobs(id INT PRIMARY KEY AUTO_INCREMENT, applied BOOLEAN DEFAULT FALSE, direct BOOLEAN,  title VARCHAR(255), company VARCHAR(255), summary TEXT(1000), url TEXT(1000))"
-
-  # Execute the sqlQuery
-  cursorObject.execute(sqlQuery)
-
-  for job in jobs:
-    # checkt to make sure jobs are unique by varifying url
-    existsSql = "SELECT COUNT(*) AS count FROM Jobs WHERE url = '%s'" % (
-        job[5])
-    cursorObject.execute(existsSql)
-    dup = cursorObject.fetchone()[0]
-    # if the url is unique add it to the database and the company is not revature
-    if(dup == 0 and job[3] != "Revature"):
-      sql = 'INSERT into Jobs(direct, title, company, summary, url) VALUES(%s, "%s", "%s", "%s", "%s");' % (
-          job[1], job[2], job[3], job[4], job[5])
-      cursorObject.execute(sql)
-      # connection is not autocommit by default. So you must commit to save
-      # your changes.
-      connectionObject.commit()
-
-
+# catch exceptions
 except Exception as e:
+    # If there is an error print it to the console
+    print("Exeception occured:{}".format(e))
 
-  print("Exeception occured:{}".format(e))
-
+# When all is done, close the sql connection
 finally:
-
-  connectionObject.close()
+    connectionObject.close()
